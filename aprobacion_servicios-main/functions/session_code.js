5b67e4783c9051f876e37d9cd3e1941133e65d73
DataSoftwarePrecimec <sistemas@quirurgicos.co>
2026-02-26 09:20:57 -0500

    Delete aprobacion_servicios-main directory

--- aprobacion_servicios-main/functions/session_code.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestPost(context) {
-  const sessionCode = crypto.randomUUID();
-  const headers     = new Headers();
-  headers.set('Set-Cookie',`session_code=${sessionCode}; HttpOnly; Secure; Path=/; SameSite=Strict`);
-  return new Response(null, { status: 204, headers });
-}
